0b44abed2ccd5f2f7cb2348a8b847b153e6d946c
sguett <guetta.samuel@gmail.com>
2020-05-06 15:56:47 +0200

    daily objects & loop

--- 2.bootcamp/Week3/Day3/DailyChallenge/script.js
+++ 2.bootcamp/Week3/Day3/DailyChallenge/script.js
@@ -15,3 +15,28 @@ if (mySub1 == -1 || mySub2 == -1) {
 } else if (mySub1 < mySub2) {
     console.log(var1.replace(var1.slice(mySub1, mySub2 + (var1.length - mySub2)), "good!"));
 }
+
+
+// Daily challenge : Objects & Loop
+
+const arr = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8];
+let swap = 0;
+for (i = 0; i < arr.length; i++) {
+    for (j = 1; j < arr.length; j++) {
+        if (arr[j - 1] < arr[j]) {
+            swap = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = swap;
+        }
+    }
+}
+
+let arrString = arr.toString();
+let joinString = arr.join("+");
+console.log(joinString);
+
+let joinString = arr.join("-");
+console.log(joinString);
+
+let joinString = arr.join(" ");
+console.log(joinString);
