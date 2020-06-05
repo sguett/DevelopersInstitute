5066dc419b12e16ce48a4a01168c8656ed88011f
Samuel Guetta <sguetta@quinten.io>
2020-06-05 14:53:18 +0200

    w7d5 daily

---
+++ 2.bootcamp/Week7/Day5/DailyChallenge/script.js
@@ -0,0 +1,32 @@
+// reverseArray([1,2,3,4,5]) // [5,4,3,2,1]
+// reverseArray([1,2]) // [2,1]
+// reverseArray([]) // []
+// reverseArray([1,2,3,4,5,6,7,8,9,10]) // [10,9,8,7,6,5,4,3,2,1]
+
+// method 1
+function reverseArray(arr){
+  return arr.reverse();
+}
+reverseArray([1,2,3,4,5,6,7,8,9,10])
+
+// method 2
+function reverseArray2(arr){
+  var newArray = [];
+  for (var i = arr.length - 1; i >= 0; i--) {
+    newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+reverseArray2([1,2,3,4,5,6,7,8,9,10])
+
+// method 3
+
+function reverseArray3(arr){
+  let reversedArray = [];
+  return arr.reduce( (reversedArray, value) => {
+    return [value , ...reversedArray];
+  }, reversedArray);
+};
+
+reverseArray3([1,2,3,4,5,6,7,8,9,10])
